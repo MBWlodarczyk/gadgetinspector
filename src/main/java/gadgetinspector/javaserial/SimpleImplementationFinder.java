64c48969c3ed30dc67761b74293afcc67e45eec3
sig1nt <ngonella@calpoly.edu>
2021-07-02 10:39:22 -0700

    Memoize simple implementation finder

--- src/main/java/gadgetinspector/javaserial/SimpleImplementationFinder.java
+++ src/main/java/gadgetinspector/javaserial/SimpleImplementationFinder.java
@@ -5,6 +5,7 @@ import gadgetinspector.SerializableDecider;
 import gadgetinspector.data.MethodReference;
 
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,14 +13,21 @@ public class SimpleImplementationFinder implements ImplementationFinder  {
 
     private final SerializableDecider serializableDecider;
     private final Map<MethodReference.Handle, Set<MethodReference.Handle>> methodImplMap;
+    private Map<MethodReference.Handle, Set<MethodReference.Handle>> getImplMemo;
 
     public SimpleImplementationFinder(SerializableDecider serializableDecider, Map<MethodReference.Handle, Set<MethodReference.Handle>> methodImplMap) {
         this.serializableDecider = serializableDecider;
         this.methodImplMap = methodImplMap;
+        this.getImplMemo = new HashMap<>();
     }
 
     @Override
     public Set<MethodReference.Handle> getImplementations(MethodReference.Handle target) {
+        Set<MethodReference.Handle> memoEntry = this.getImplMemo.get(target);
+        if (memoEntry != null) {
+           return memoEntry;
+        }
+
         Set<MethodReference.Handle> allImpls = new HashSet<>();
 
         // Assume that the target method is always available, even if not serializable; the target may just be a local
@@ -35,6 +43,7 @@ public class SimpleImplementationFinder implements ImplementationFinder  {
             }
         }
 
+        this.getImplMemo.put(target, allImpls);
         return allImpls;
     }
 }
